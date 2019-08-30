17e0967ca46815553d29be38e31c71adb7612146
Ricardo - Main PC <37965676+RicardoFernandez286@users.noreply.github.com>
2019-08-30 16:17:14 +0200

    Update batch2DGC_fit.m

--- 2D-IR/Analysis subroutines/batch2DGC_fit.m
+++ 2D-IR/Analysis subroutines/batch2DGC_fit.m
@@ -1,7 +1,7 @@
 function batch2DGC_fit(rootfolder,fitType,varargin)
 %% Define startup variables
-rootfolder  = 'D:\Ricardo Data\switchdrive\Ph.D. UZH\MANUSCRIPTS\9) 2D IR distance - na\Latest Simulations\Dimer_distance1';
-fitType     = 'Test'; % 'Ricardo' or 'Andrea'
+% rootfolder  = 'D:\Ricardo Data\switchdrive\Ph.D. UZH\MANUSCRIPTS\9) 2D IR distance - na\Latest Simulations\Dimer_distance1';
+% fitType     = 'Test'; % 'Ricardo' or 'Andrea'
 
 %% Start parallel pool (if none exists)
 if isempty(gcp('nocreate'))
@@ -119,4 +119,4 @@ for i=1:Ndatafiles
     end
 end
 
-% exit
+exit
