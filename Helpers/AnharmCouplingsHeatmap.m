ab8e6277816ce3e80cb7bbc2b8db4095fbffcf47
Ricardo - Main PC <37965676+RicardoFernandez286@users.noreply.github.com>
2021-06-16 23:34:36 +0100

    new features

--- Helpers/AnharmCouplingsHeatmap.m
+++ Helpers/AnharmCouplingsHeatmap.m
@@ -1,8 +1,20 @@
+function AnharmCouplingsHeatmap
+
 Nmodes = 4;
 ModeLabels = ["A'(2)" "A''" "A'(1)" "C{\equiv}C"];
 
 molec = 'NMe2';
+cmapID = 'Blues5';
+NanCol  = 1*[1 1 1];
+LineCol = 1*[1 1 1];
+LineWidth = 1.5;
+LineStyle = 'solid'; % 'solid' | 'dashed' | 'dotted' | 'dashdot' |'none'
 
+PlotW1W3  = 1; % 0 = energy ordering, 1 = 2D-IR like
+plotTriang= -1; % 0 = plot everything; 1 = upper; -1 = lower;
+hideDiag  = 0;
+
+%% Frequency input
 switch molec
     case 'NMe2'
 freqs = [...
@@ -64,28 +76,141 @@ H2 = triu(H2.',1) + tril(H2);
 
 anh = H1-H2;
 
-
-
 %% Plot and beautify
-fh1 = figure(1);
-clf(fh1);
+fh = figure(1);
+fh.Color='w';
+clf(fh);
+fh.Position(3:4) = [509 420];
 
-anh(ii<jj) = nan; % To remove the upper block
-% anh(ii==jj) = nan; % To remove diagonal
+switch plotTriang % Plot a triangular matrix
+    case -1
+        if PlotW1W3 == 1
+            anh(ii>jj) = nan; 
+        else
+            anh(ii<jj) = nan; 
+        end
+    case 1
+        if PlotW1W3 == 1
+            anh(ii<jj) = nan; 
+        else
+            anh(ii>jj) = nan; 
+        end
+end
 
-NanCol  = 0.9*[1 1 1];
-LineCol = 0.9*[1 1 1];
-h_map = heatmap(fh1,anh,'MissingDataLabel','','CellLabelFormat','%0.1f','MissingDataColor',NanCol,'FontSize',16);
-colormap(othercolor('Blues9',Nmodes));
-% colormap(othercolor('Reds9'));
-% colormap(othercolor('Oranges9'));
+if hideDiag == 1
+    anh(ii==jj) = nan; % To remove diagonal
+end
 
+if PlotW1W3 == 1
+    anh = flipud(anh);
+end
+
+h_map = heatmap(fh,anh,'MissingDataLabel','','CellLabelFormat','%0.1f','MissingDataColor',NanCol,'FontSize',16);
+colormap(othercolor(cmapID,Nmodes*5));
 
 warning('off','MATLAB:structOnObject');
 
 hHeatmap = struct(h_map).Heatmap;
 hGrid = struct(hHeatmap).Grid;
 hGrid.ColorData = uint8(256*[LineCol 0.5]');
+hGrid.LineWidth = LineWidth;
+hGrid.LineStyle = LineStyle;
 
 h_map.XData = ModeLabels;
-h_map.YData = ModeLabels;
+if PlotW1W3 == 1
+    h_map.YData = fliplr(ModeLabels);
+else
+    h_map.YData = ModeLabels;
+end
+
+%% Make it square
+
+% Temporarily change axis units 
+originalUnits = h_map.Units;  % save original units (probaly normalized)
+h_map.Units = 'centimeters';  % any unit that will result in squares
+% Get number of rows & columns
+sz = size(h_map.ColorData); 
+% Change axis size & position;
+originalPos = h_map.Position; 
+% make axes square (not the table cells, just the axes)
+h_map.Position(3:4) = min(h_map.Position(3:4))*[1,1]; 
+if sz(1)>sz(2)
+    % make the axis size more narrow and re-center
+    h_map.Position(3) = h_map.Position(3)*(sz(2)/sz(1)); 
+    h_map.Position(1) = (originalPos(1)+originalPos(3)/2)-(h_map.Position(3)/2);
+else
+    % make the axis size shorter and re-center
+    h_map.Position(4) = h_map.Position(4)*(sz(1)/sz(2));
+    h_map.Position(2) = (originalPos(2)+originalPos(4)/2)-(h_map.Position(4)/2);
+end
+% Return axis to original units
+h_map.Units = originalUnits; 
+
+% % Temporarily change figure units 
+% originalUnits_fig = fh.Units;  % save original units (probaly normalized)
+% originalUnits_ax = h_map.Units; 
+% fh.Units = 'centimeters';  % any unit that will result in squares
+% h_map.Units = 'Normalized'; 
+% % Get number of rows & columns
+% sz = size(h_map.ColorData); 
+% % make axes square (not the table cells, just the axes)
+% h_map.Position(3:4) = min(h_map.Position(3:4))*[1,1]; 
+% fh.InnerPosition(3:4) = min(fh.InnerPosition(3:4))*[1,1]; 
+% % Change figure position;
+% if sz(1)>sz(2)
+%     % make the figure size more narrow
+%     fh.InnerPosition(3) = fh.InnerPosition(3)*(sz(2)/sz(1)); 
+% else
+%     % make the figure size shorter
+%     fh.InnerPosition(4) = fh.InnerPosition(4)*(sz(1)/sz(2)); 
+% end
+% % return original figure units
+% fh.Units = originalUnits_fig; 
+% h_map.Units = originalUnits_ax; 
+
+% % Put an axis over top of the heatmap
+% % The axis will not be visible but will cover all area 
+% % to the right of the heatmap. 
+% hmp = h_map.Position; 
+% cbax = axes('Position',[sum(hmp([1,3])), 0, 1-sum(hmp([1,3])), 1],...
+%     'XTick',[], 'YTick', [], 'Color',fh.Color);
+% cbax.XAxis.Visible = 'off';
+% cbax.YAxis.Visible = 'off';
+% % Set the new axis color map to match the 
+% % heatmap's colormap
+% cbax.Colormap = h_map.Colormap; 
+% % Add a colorbar the same vertical position as the heatmap
+% cbh = colorbar(cbax,'Position',[.90, hmp(2), .03, hmp(4)]); 
+% % Set the limits to 0:1 and set the ticks so that they 
+% % are in the center of each bar
+% cbh.Limits = [0,1]; 
+% nColors = size(h_map.Colormap,1); 
+% cbh.Ticks = (1/nColors : 1/nColors : 1) - 1/nColors/2; 
+% % Set the new labels for each tick
+% cbh.TickLabels = 0:nColors-1; 
+% % Set the colorbar fontsize to the same value as heatmap fontsize
+% cbh.FontSize = h_map.FontSize;
+
+% % Temporarily change figure units 
+% originalUnits_fig = fh.Units;  % save original units (probaly normalized)
+% originalUnits_ax = h_map.Units; 
+% fh.Units = 'centimeters';  % any unit that will result in squares
+% h_map.Units = 'Normalized'; 
+% % Get number of rows & columns
+% sz = size(h_map.ColorData); 
+% % make axes square (not the table cells, just the axes)
+% h_map.Position(3:4) = min(h_map.Position(3:4))*[1,1]; 
+% fh.InnerPosition(3:4) = min(fh.InnerPosition(3:4))*[1,1]; 
+% % Change figure position;
+% if sz(1)>sz(2)
+%     % make the figure size more narrow
+%     fh.InnerPosition(3) = fh.InnerPosition(3)*(sz(2)/sz(1)); 
+% else
+%     % make the figure size shorter
+%     fh.InnerPosition(4) = fh.InnerPosition(4)*(sz(1)/sz(2)); 
+% end
+% % return original figure units
+% fh.Units = originalUnits_fig; 
+% h_map.Units = originalUnits_ax;
+
+end
---
+++ Steady-state/AutolabEChem.m
@@ -0,0 +1,426 @@
+% function EChem()
+% CURRENT VERSION: v1.1a
+% 04.12.2019 / Ricardo Fernandez-Teran
+% Changelog:
+% - Added automatic peak detection and possibility to correct errors, as well as automated Randles-Sevcik and Trumpet plots.
+% - Added compatibility with Ivium data (XY XY XY XY format). Can discriminate in the case E I Q was saved instead of XY XY XY
+% - Updated potential referencing and unit conversion
+% - Other major improvements
+
+%% Change the input units here
+inI_units   = 'A';      % Options: 'mA', 'microA' or 'A'
+inE_units   = 'V';      % Options: 'mV' or 'V'
+DataType    = 'Nova';   % Options: 'EC-lab' or 'Ivium'
+
+%% Sort by scan rate?
+SortScanRate = 1;
+
+%% Plotting options
+WhichScan   = 1;
+LineWidth   = 1.5;
+
+%% Get the directory and a list of .txt files to load and plot
+rootdir     = uigetdir;
+
+if rootdir == 0
+    return
+end
+
+filelist    = dir(rootdir);
+filenames   = {filelist.name}';
+filenames   = filenames(contains(filenames,'.dat','IgnoreCase',true));
+[filenames_idx,filesSelected] = listdlg('ListString',filenames,'SelectionMode','multiple','ListSize',[400,300],'PromptString','Select files to plot and load:');
+
+if filesSelected == 0
+    return
+end
+
+ref_type    = questdlg('Which internal reference did you use?','Select internal reference','Ferrocene','Fc(Cp*)2','None/Other','Ferrocene');
+
+switch ref_type
+    case 'Ferrocene'
+        ref_pot         = inputdlg('Ferrocene half-potential (mV):','Define potential scale',[1,50],{'0'});
+        if(isempty(ref_pot))
+            return
+        end
+        ref_pot_mV      = str2num(ref_pot{1});
+        ref_pot_name    = 'Fc/Fc^+';
+    case 'Fc(Cp*)2'
+        ref_pot         = inputdlg({'Reference couple half-potential (mV):';'Potential of Ferrocene in your solvent (mV vs. Fc(Cp*)2):'},'Define potential scale',[1,50],{'0';'MeCN=505; THF=427'});
+        if(isempty(ref_pot))
+            return
+        end
+        ref_pot_mV      = str2num(ref_pot{1})+str2double(ref_pot{2});
+        ref_pot_name    = 'Fc/Fc^+';
+    case 'None/Other'
+        ref_pot         = inputdlg({'Reference couple half-potential (mV):';'Reference couple name:'},'Define potential scale',[1,50],{'0';'Fc/Fc^+'});
+        if(isempty(ref_pot))
+            return
+        end
+        ref_pot_mV      = str2num(ref_pot{1});
+        ref_pot_name    = ref_pot{2};
+end
+
+
+%% Define plotting units and options
+
+% Change units
+switch inI_units
+    case 'microA'
+        outI_factor = 1e-3;
+    case 'mA'
+        outI_factor = 1;
+    case 'A'
+        outI_factor = 1e3;
+end
+
+switch inE_units
+    case 'mV'
+        outE_factor = 1;
+    case 'V'
+        outE_factor = 1000;
+end
+
+outI_units  = 'mA';   % set the legend of the current axis
+
+if ~isempty(ref_pot_name)
+    outE_units  = ['mV vs. ' ref_pot_name];     % set the legend of the potential axis (reference comes later)
+else
+    outE_units  = 'mV' ;     % set the legend of the potential axis (reference comes later)
+end
+
+%% Load the files and separate into cell array CV data vs scan number
+% Data structure: Cell array(File index, ScanNo) / [E x I]
+Nfiles      = length(filenames_idx);
+Nscans      = zeros(Nfiles,1);
+AllData     = cell(Nfiles,1);
+caption     = cell(Nfiles,1);
+
+switch DataType
+    case 'Ivium'
+        for i=1:Nfiles
+            % Read from file
+            data        = readmatrix([rootdir filesep filenames{filenames_idx(i)}],'NumHeaderLines',1);
+            % Remove NaN columns
+            data        = data(:,any(~isnan(data), 1));
+            % Sort into {[E x I]} vs scan number
+            Nscans(i)   = floor(size(data,2)/2); 
+            
+            % Sort into {[E x I]} vs scan number
+            for j=1:Nscans(i)
+                data(:,2*j-1)   = data(:,2*j-1)*outE_factor - ref_pot_mV;
+                data(:,2*j)     = data(:,2*j)*outI_factor;
+                AllData{i,j}    = data(:,(2*j-1):2*j);
+            end
+            % Get a caption string
+            [~,caption{i},~]    = fileparts(filenames{filenames_idx(i)});
+        end
+    case 'EC-lab'
+        for i=1:Nfiles
+            % Read from file
+            data        = readmatrix([rootdir filesep filenames{filenames_idx(i)}],'NumHeaderLines',1);
+            data(:,1)   = data(:,1)*outE_factor - ref_pot_mV;
+            data(:,2)   = data(:,2)*outI_factor;
+            % Sort into {[E x I]} vs scan number
+            Nscans(i)   = max(data(:,3)); 
+            for j=1:Nscans
+                AllData{i,j}    = data(data(:,3)==j,1:2);
+            end
+            % Get a caption string
+            [~,caption{i},~]    = fileparts(filenames{filenames_idx(i)});
+        end
+    case 'Nova'
+        for i=1:Nfiles
+            % Read from file
+            data        = readmatrix([rootdir filesep filenames{filenames_idx(i)}],'NumHeaderLines',1);
+            data(:,1)   = data(:,2)*outE_factor - ref_pot_mV;
+            data(:,2)   = data(:,3)*outI_factor;
+            % Sort into {[E x I]} vs scan number
+            Nscans(i)   = max(data(:,4)); 
+            for j=1:Nscans
+                AllData{i,j}    = data(data(:,4)==j,1:2);
+            end
+            % Get a caption string
+            [~,caption{i},~]    = fileparts(filenames{filenames_idx(i)});
+        end
+end
+
+%% Sort scan rate dependence
+if SortScanRate == 1
+    ScanRates_num       = zeros(Nfiles,1);
+    for i=1:Nfiles
+        spltCaption = strsplit(caption{i},'_');
+        ScanRates_st{i} = spltCaption{end-1};
+        ScanRates_num(i)= str2double(ScanRates_st{i});
+        if ScanRates_num(i) < 1
+            caption{i}      = [num2str(ScanRates_num(i)*1000) ' mV/s'];
+        else
+            caption{i}      = [num2str(ScanRates_num(i)) ' V/s'];
+        end
+    end
+    
+    [sort_SR,sort_id]   = sort(ScanRates_num,'ascend');
+    AllData             = AllData(sort_id(sort_SR > 0 & ~isnan(sort_SR)),:);
+    caption             = caption(sort_id(sort_SR > 0 & ~isnan(sort_SR)));
+    Nscans              = Nscans(sort_id(sort_SR > 0 & ~isnan(sort_SR)));
+    ScanRates_num       = ScanRates_num(sort_id(sort_SR > 0 & ~isnan(sort_SR)));
+    Nfiles              = size(AllData,1);
+end
+
+%% Plot the data
+fh = figure();
+clf(fh);
+ax = axes('parent',fh);
+
+%%% Format figure and plots
+% Figure format
+fh.Color    = 'w';
+fh.Position = [300,600,900,420];
+
+% Axis format
+legend(ax,'show');
+legend(ax,'location','eastoutside');
+legend(ax,'interpreter','none');
+legend(ax,'boxoff');
+ax.Position = [0.07 0.1475 0.75 0.8];
+
+box(ax,'on');
+ax.FontSize = 14;
+xlabel(ax,['Potential (' outE_units ')'],'FontWeight','bold');
+ylabel(ax,['Current (' outI_units ')'],'FontWeight','bold');
+axis(ax,'tight');
+
+% Add XY lines
+xl = xline(ax,0,'HandleVisibility','off'); xl.Color = 0.75*[1 1 1];
+yl = yline(ax,0,'HandleVisibility','off'); yl.Color = 0.75*[1 1 1];
+
+hold(ax,'on')
+if SortScanRate == 1
+    cmap = colormap(ax,othercolor('Mrainbow',Nfiles));
+    for i=1:Nfiles
+        plotScan = min(Nscans(i),WhichScan);
+        plot(AllData{i,plotScan}(:,1),AllData{i,plotScan}(:,2),'DisplayName',caption{i},'Color',cmap(i,:),'LineWidth',LineWidth)
+%         pause(0.1);
+%         drawnow;
+    end
+else
+    for i=1:Nfiles
+        plotScan = min(Nscans(i),WhichScan);
+        plot(AllData{i,plotScan}(:,1),AllData{i,plotScan}(:,2),'DisplayName',caption{i},'LineWidth',LineWidth)
+    end
+end
+hold(ax,'off');
+
+%% Do the peak analysis
+if SortScanRate ~= 1
+    return
+end
+
+doAnalysis = questdlg('Do you want to perform scan-rate dependence analysis?','Scan-rate analysis','Yes','No','No');
+if isempty(doAnalysis) || strcmp(doAnalysis,'No') 
+    return
+end
+pkCurr          = zeros(Nfiles,2);
+pkPot           = zeros(Nfiles,2);
+E12             = zeros(Nfiles,1);
+fw_E            = cell(Nfiles,1);
+fw_I            = cell(Nfiles,1);
+bw_E            = cell(Nfiles,1);
+bw_I            = cell(Nfiles,1);
+fitrange        = inputdlg('Select range for peak analysis (start,end in mV):','Select range...');
+if isempty(fitrange)
+    return
+end
+fitrange        = str2num(fitrange{1});
+
+for i=1:Nfiles
+    % First: Separate the scan in forward/backward halves
+    AllE            = AllData{i,plotScan}(:,1);
+    AllI            = AllData{i,plotScan}(:,2);
+    InRange         = AllE>=min(fitrange) & AllE<=max(fitrange);
+    AllE            = AllE(InRange);
+    AllI            = AllI(InRange);
+    [~,idx_max]     = max(AllE);
+    [~,idx_min]     = min(AllE);
+    Npoints         = length(AllE);
+    idx             = 1:Npoints;
+    if idx_max > idx_min % if min comes first then 1st half is reductive
+        fw_pts      = [1:idx_min (idx_max+1):Npoints];
+        bw_pts      = (idx_min+1):idx_max;
+    else
+        bw_pts      = [1:idx_max (idx_min+1):Npoints];
+        fw_pts      = (idx_max+1):idx_min;
+    end
+    slope           = zeros(Npoints,1);
+    slope(fw_pts)   = +1;
+    slope(bw_pts)   = -1;
+    % Second: Fit peaks on the forward/backward scans
+    %%% Forward scan (reductive)
+    [fw_E{i},idx_fw_E]  = sort(AllE(slope>0));
+    [fw_E{i},un_fw_E]   = unique(fw_E{i});          
+    fw_I{i}             = AllI(slope>0);
+    fw_I{i}             = fw_I{i}(idx_fw_E(un_fw_E));
+    Norm_fw             = min(fw_I{i});
+    [pk_I,pk_E]         = findpeaks(fw_I{i}/Norm_fw,fw_E{i},'MinPeakHeight',0.1,'MinPeakDistance',50,'NPeaks',1);
+    if ~isempty(pk_I)
+        pkCurr(i,1)     = pk_I*Norm_fw;
+        pkPot(i,1)      = pk_E;
+    else
+        pkCurr(i,1)     = 0;
+        pkPot(i,1)      = 0;
+    end
+    %%% Backward scan (oxidative)
+    [bw_E{i},idx_bw_E]  = sort(AllE(slope<0));
+    [bw_E{i},un_bw_E]   = unique(bw_E{i});
+    bw_I{i}             = AllI(slope<0);
+    bw_I{i}             = bw_I{i}(idx_bw_E(un_bw_E));
+    Norm_bw             = max(bw_I{i});
+    [pk_I,pk_E]         = findpeaks(bw_I{i}/Norm_bw,bw_E{i},'MinPeakHeight',0.1,'MinPeakDistance',50,'NPeaks',1);
+    if ~isempty(pk_I)
+        pkCurr(i,2)     = pk_I*Norm_bw;
+        pkPot(i,2)      = pk_E;
+    else
+        pkCurr(i,2)     = 0;
+        pkPot(i,2)      = 0;
+    end
+    %%% E1/2
+    E12(i)          = mean(pkPot(i,:));
+end
+
+%% Add peak positions to main plot
+hold(ax,'on')
+for i=1:Nfiles
+    plot(pkPot(i,1),pkCurr(i,1),'o','Color',cmap(i,:),'HandleVisibility','off');
+    plot(pkPot(i,2),pkCurr(i,2),'o','Color',cmap(i,:),'HandleVisibility','off');
+end
+hold(ax,'off')
+
+%% Make Trumpet plot
+fh_tr = figure('Name','Trumpet plot','NumberTitle','off');
+fh_tr.Color = 'w';
+ax_tr = axes('parent',fh_tr);
+ax_tr.FontSize = 14;
+box(ax_tr,'on');
+hold(ax_tr,'on')
+plot(ax_tr,ScanRates_num,pkPot(:,2)-E12,'^r','MarkerSize',8,'DisplayName','Anodic peak (oxidation)')
+plot(ax_tr,ScanRates_num,pkPot(:,1)-E12,'vb','MarkerSize',8,'DisplayName','Cathodic peak (reduction)')
+yline(0,'HandleVisibility','off');
+legend(ax_tr,'show');
+legend(ax_tr,'boxoff');
+legend(ax_tr,'location','northoutside','Orientation','horizontal')
+xlabel(ax_tr,'Scan rate (mV s^{-1})','FontWeight','bold')
+ylabel(ax_tr,'E_{peak} - E_{1/2} (mV)','FontWeight','bold')
+
+hold(ax_tr,'off')
+ax_tr.XScale = 'log';
+
+%% Make Randles-Sevcik plot
+fh_rs = figure('Name','Randles-Sevcik plot','NumberTitle','off');
+fh_rs.Color = 'w';
+ax_rs = axes('parent',fh_rs);
+ax_rs.FontSize = 14;
+box(ax_rs,'on');
+hold(ax_rs,'on')
+plot(ax_rs,sqrt(ScanRates_num),abs(pkCurr(:,2)),'^r','MarkerFaceColor','r','MarkerSize',8,'DisplayName','Anodic current (oxidation)')
+plot(ax_rs,sqrt(ScanRates_num),abs(pkCurr(:,1)),'vb','MarkerFaceColor','b','MarkerSize',8,'DisplayName','Cathodic current (reduction)')
+
+pA = polyfit(sqrt(ScanRates_num),abs(pkCurr(:,2)),1);
+pC = polyfit(sqrt(ScanRates_num),abs(pkCurr(:,1)),1);
+
+xplt = [min(sqrt(ScanRates_num))*0.85 max(sqrt(ScanRates_num))*1.15];
+plot(xplt,polyval(pA,xplt),'r-','LineWidth',1,'HandleVisibility','off');
+plot(xplt,polyval(pC,xplt),'b-','LineWidth',1,'HandleVisibility','off');
+
+legend(ax_rs,'show');
+legend(ax_rs,'boxoff');
+legend(ax_rs,'location','northoutside','Orientation','horizontal')
+xlabel(ax_rs,'(Scan rate)^{1/2} (mV s^{-1})^{1/2}','FontWeight','bold')
+ylabel(ax_rs,['Peak current (' outI_units ')'],'FontWeight','bold')
+axis(ax_rs,'tight');
+hold(ax_rs,'off')
+
+%% Make ipc/ipa plot
+fh_rv = figure('Name','Reversibility plot','NumberTitle','off');
+fh_rv.Color = 'w';
+ax_rv = axes('parent',fh_rv);
+ax_rv.FontSize = 14;
+box(ax_rv,'on');
+hold(ax_rv,'on')
+plot(ax_rv,ScanRates_num,abs(pkCurr(:,1)./pkCurr(:,2)),'or','MarkerSize',8)
+xlabel(ax_rv,'Scan rate (mV s^{-1})','FontWeight','bold')
+ylabel(ax_rv,'i_{pc}/i_{pa}','FontWeight','bold')
+hold(ax_rv,'off')
+ax_rv.XScale = 'log';
+
+return;
+
+%% Custom points (Fix when the program gets the wrong potential)
+% Just need to find the right potential, it will get the current automatically
+% index: (a,b)   a=Which file;  b=1 for Cathodic (negative), b=2 for Anodic (positive) peaks
+a       = 1:7;            %#ok<*UNRCH>
+b       = 2;
+% NewVal  = flip([-67.72 -41.78 -2.722 31.6 85 224 361]); % New X value for the min/max that we're changing
+
+NewVal  = [1084 1213 1376 1633 1683 1730 1818];
+
+%%% Do not edit below this line
+for q = 1:length(a)
+    switch b
+        case 1
+            Xval = fw_E{q};
+            Yval = fw_I{q};
+        case 2
+            Xval = bw_E{q};
+            Yval = bw_I{q};
+    end
+    idx_new = findClosestId2Val(Xval,NewVal(q));
+
+    pkPot(q,b) = NewVal(q);
+    pkCurr(q,b)= Yval(idx_new);
+end
+    E12        = mean(pkPot,2);
+
+    return
+%% Plot Derivative
+fh = figure();
+clf(fh);
+ax = axes('parent',fh);
+
+%%% Format figure and plots
+% Figure format
+fh.Color    = 'w';
+fh.Position = [300,600,900,420];
+
+% Axis format
+legend(ax,'show');
+legend(ax,'location','eastoutside');
+legend(ax,'interpreter','none');
+legend(ax,'boxoff');
+ax.Position = [0.07 0.1475 0.75 0.8];
+
+box(ax,'on');
+ax.FontSize = 14;
+xlabel(ax,['Potential (' outE_units ')'],'FontWeight','bold');
+ylabel(ax,['Current (' outI_units ')'],'FontWeight','bold');
+axis(ax,'tight');
+
+% Add XY lines
+xl = xline(ax,0,'HandleVisibility','off'); xl.Color = 0.75*[1 1 1];
+yl = yline(ax,0,'HandleVisibility','off'); yl.Color = 0.75*[1 1 1];
+
+hold(ax,'on')
+if SortScanRate == 1
+    cmap = colormap(ax,othercolor('Mrainbow',Nfiles));
+    for i=1:Nfiles
+        plotScan = min(Nscans(i),WhichScan);
+        plot(AllData{i,plotScan}(2:end,1),smooth(diff(AllData{i,plotScan}(:,2))),'DisplayName',caption{i},'Color',cmap(i,:),'LineWidth',LineWidth)
+%         pause(0.1);
+%         drawnow;
+    end
+else
+    for i=1:Nfiles
+        plotScan = min(Nscans(i),WhichScan);
+        plot(AllData{i,plotScan}(2:end,1),smooth(diff(AllData{i,plotScan}(:,2))),'DisplayName',caption{i},'LineWidth',LineWidth)
+    end
+end
+hold(ax,'off');
